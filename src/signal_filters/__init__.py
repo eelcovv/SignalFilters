64ecc321c383b9d3c3b2dd35a773f96510174aea
EVLT <evlt@cbs.nl>
2024-04-12 22:55:57 +0200

    reformatted examples

--- src/signal_filters/__init__.py
+++ src/signal_filters/__init__.py
@@ -4,6 +4,7 @@ if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 else:
+    # noinspection PyUnresolvedReferences
     from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
